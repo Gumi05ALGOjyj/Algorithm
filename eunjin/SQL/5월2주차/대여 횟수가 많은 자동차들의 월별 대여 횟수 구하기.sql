62291e265d073d3db579e92085f27c17aac1e7fe
SilverStar <98890934+thisiseunjin@users.noreply.github.com>
2023-05-10 23:19:30 +0900

    Update 대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기.sql(안됌)

--- eunjin/SQL/5월2주차/대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기.sql
+++ eunjin/SQL/5월2주차/대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기.sql
@@ -1,4 +1,4 @@
-select MONTH(START_DATE) as MONTH, CAR_ID as CAR_ID, count(HISTORY_ID) as RECORDS
+select MONTH(START_DATE) as MONTH, CAR_ID as CAR_ID, count(HISTORY_ID) as RECORDS  
 from 
 (select * from CAR_RENTAL_COMPANY_RENTAL_HISTORY group by car_id having start_date between '2022-08-01' and
     '2022-10-31' and count(*)>4) tmp
