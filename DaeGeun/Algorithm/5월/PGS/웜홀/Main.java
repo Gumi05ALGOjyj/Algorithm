b9ae5a04e18f13594fee3678a15b82d28b08d2e5
D__G_UN <eorms96@naver.com>
2023-05-18 09:11:30 +0900

    웜홀 주석수정

--- DaeGeun/Algorithm/5월/PGS/웜홀/Main.java
+++ DaeGeun/Algorithm/5월/PGS/웜홀/Main.java
@@ -8,12 +8,12 @@ public class Main {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		final int NONE = 25_000_001;
-		int T = Integer.parseInt(br.readLine());
 		
 		StringTokenizer st;
 		int N,M,W;
 		int[][] map;
 		TC:
+		int T = Integer.parseInt(br.1readLine());
 		for(int test_case = 1; test_case <= T ; test_case++) {
 			st = new StringTokenizer(br.readLine());
 			N = Integer.parseInt(st.nextToken());
@@ -24,22 +24,24 @@ public class Main {
 			
 			{ // 입력처리 - map 초기화
 				int from, to, cost;
+				//다리 입력
 				for(int i=0;i<M;i++) {
 					st = new StringTokenizer(br.readLine());
-					from = Integer.parseInt(st.nextToken())-1;
-					to = Integer.parseInt(st.nextToken())-1;
+					from = Integer.parseInt(st.nextToken())-1; //번호 to index
+					to = Integer.parseInt(st.nextToken())-1; //번호 to index
 					cost = Integer.parseInt(st.nextToken());
 					
+					//다리가 여러 개 있을 수도 있으니 최소값으로 입력
 					if(cost<map[from][to]) {
 						map[from][to]=cost;
 						map[to][from]=cost;
 					}
 				}
-				
+				//웜홀 입력
 				for(int i=0;i<W;i++) {
 					st = new StringTokenizer(br.readLine());
-					from = Integer.parseInt(st.nextToken())-1;
-					to = Integer.parseInt(st.nextToken())-1;
+					from = Integer.parseInt(st.nextToken())-1; //번호 to index
+					to = Integer.parseInt(st.nextToken())-1; //번호 to index
 					cost = Integer.parseInt(st.nextToken());
 					
 					map[from][to]= Math.min(map[from][to], cost*-1);
@@ -56,7 +58,7 @@ public class Main {
 					}
 				}
 			}
-      //2차진행. 한번이라도 갱신이 일어나면 YES출력            
+      			//2차진행. 한번이라도 갱신이 일어나면 YES출력            
 			for(int i=0;i<N;i++) {
 				for(int from=0;from<N;from++) {
 					for(int to=0;to<N;to++) {
